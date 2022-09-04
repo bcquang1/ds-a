f03d17b1d5a34cc7ece27dd09da354680f6a6371
bcquangg <94882579+bcquangg@users.noreply.github.com>
2022-09-04 17:19:09 +0700

    Add files via upload

---
+++ binary_list/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 20
+
+int cnt,n,x[N];
+int check(int v, int k)
+{
+    return 1;
+}
+
+void solution()
+{
+    cnt+=1;
+    printf("Solution %d:", cnt);
+    for (int i=1;i<=n;i++){
+        printf("%d", x[i]);
+    }
+    printf("\n");
+}
+
+void Try(int k)
+{
+    for (int v=0;v<=1;v++){
+        if (check(v,k)){
+            x[k]=v;
+            if (k==n) solution();
+            else Try(k+1);
+        }
+    }
+}
+
+
+int main()
+{
+    n=3;
+    Try(1);
+    return 0;
+}
